bbdf5ca4f2dda5f5b656eba9384467cf9e2be885
ColossalSquid-svg <idrinkwater1987@gmail.com>
2025-04-30 17:26:45 -0700

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -1,1 +1,35 @@
+// Superclass
+class Animal {
+    String name;
+    public Animal(String name) {
+        this.name = name;
+    }
+    public void eat() {
+        System.out.println(name + " is eating.");
+    }
+}
+// Subclass inheriting from Animal
+class Dog extends Animal {
+    public Dog(String name) {
+        super(name); // Call superclass constructor
+    }
+    public void bark() {
+        System.out.println(name + " is barking.");
+    }
+    //Method overriding
+    @Override
+     public void eat() {
+        System.out.println(name + " is eating bones.");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Dog myDog = new Dog("Buddy");
+        myDog.eat(); // Inherited method
+        myDog.bark(); // Method specific to Dog
+        Animal p = new Dog("Jade");
+        p.eat();
 
+      
+    }
+}
